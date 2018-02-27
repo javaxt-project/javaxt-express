95311841b010a4d65987e45a434340bb807a6e57
pborissow <peter.borissow@yahoo.com>
2018-02-27 10:45:15 +0000

    - Added 3 new classes from the Akula project.
    git-svn-id: svn://192.168.0.80/JavaXT/javaxt-express@1001 2c7b0aa6-e0b2-3c4e-bb4a-8b65b6c465ff

---
+++ src/javaxt/express/KeyManager.java
@@ -0,0 +1,104 @@
+package javaxt.express;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.security.KeyStore;
+import java.security.Principal;
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.X509ExtendedKeyManager;
+import javax.net.ssl.X509KeyManager;
+
+//******************************************************************************
+//**  KeyManager
+//******************************************************************************
+/**
+ *   Custom implementation of a X509KeyManager. This class is required to 
+ *   support keystores with multiple SSL certificates. By default, the standard
+ *   Java X509KeyManager and the SunX509 implementation will pick the first
+ *   aliases it finds for which there is a private key and a key of the right 
+ *   type for the chosen cipher suite (typically RSA). Instead, this class
+ *   relies on a map of hostnames and their corresponding IP addresses. When a
+ *   new SSL request is made, it checks the incoming IP address and finds the
+ *   corresponding hostname. Then, it tries to find an alias in the keystore 
+ *   that corresponds to the hostname.
+ *
+ ******************************************************************************/
+
+public class KeyManager extends X509ExtendedKeyManager implements X509KeyManager {
+    private KeyStore keyStore;
+    private char[] password;
+    private String alias;
+    private java.util.HashMap<InetAddress, String> aliases;
+
+    public KeyManager(KeyStore keystore, char[] password, String alias) {
+        if (alias==null) throw new IllegalArgumentException("Alias is null.");
+        this.keyStore = keystore;
+        this.password = password;
+        this.alias = alias;
+    }
+    
+    public KeyManager(KeyStore keystore, char[] password, java.util.HashMap<InetAddress, String> aliases) {
+        if (aliases==null || aliases.isEmpty()) throw new IllegalArgumentException("Hosts is null or empty.");
+        this.keyStore = keystore;
+        this.password = password;
+        this.aliases = aliases;
+    }
+    
+    public String chooseEngineServerAlias(String keyType, Principal[] issuers, SSLEngine engine) {
+        if (alias!=null) return alias;
+        else{
+            try{
+                return aliases.get(InetAddress.getByName(engine.getPeerHost()));
+            }
+            catch(Exception e){
+                return null;
+            }
+        }
+    }
+
+    public String chooseServerAlias(String keyType, Principal[] issuers, Socket socket) {
+        if (alias!=null) return alias;
+        else return aliases.get(socket.getLocalAddress());
+    }
+
+    public PrivateKey getPrivateKey(String alias) {
+        try {
+            return (PrivateKey) keyStore.getKey(alias, password);
+        } 
+        catch (Exception e) {
+            return null;
+        }
+    }
+
+    public X509Certificate[] getCertificateChain(String alias) {
+        try {
+            java.security.cert.Certificate[] certs = keyStore.getCertificateChain(alias);
+            if (certs == null || certs.length == 0) return null;
+            X509Certificate[] x509 = new X509Certificate[certs.length];
+            for (int i = 0; i < certs.length; i++){
+                x509[i] = (X509Certificate)certs[i];
+            }
+            return x509;
+        } 
+        catch (Exception e) {
+            return null;
+        }          
+    }
+
+    public String[] getServerAliases(String keyType, Principal[] issuers) {
+        throw new UnsupportedOperationException("Method getServerAliases() not yet implemented.");
+    }
+
+    public String[] getClientAliases(String keyType, Principal[] issuers) {
+        throw new UnsupportedOperationException("Method getClientAliases() not yet implemented.");
+    }
+
+    public String chooseClientAlias(String keyTypes[], Principal[] issuers, Socket socket) {
+        throw new UnsupportedOperationException("Method chooseClientAlias() not yet implemented.");
+    }
+
+    public String chooseEngineClientAlias(String[] strings, Principal[] prncpls, SSLEngine ssle) {
+        throw new UnsupportedOperationException("Method chooseEngineClientAlias() not yet implemented.");
+    }
+}
---
+++ src/javaxt/express/Logger.java
@@ -0,0 +1,147 @@
+package javaxt.express;
+import java.util.List;
+import java.util.LinkedList;
+import java.text.SimpleDateFormat;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import javaxt.http.servlet.HttpServletRequest;
+
+//******************************************************************************
+//**  Logger Class
+//******************************************************************************
+/**
+ *   Used to log server activity to a text file. A new text file is created
+ *   for each day.
+ *
+ ******************************************************************************/
+
+public class Logger implements Runnable {
+
+    private static List pool = new LinkedList();
+    private File logDir;
+    private FileChannel outChannel;
+    private FileOutputStream outputFile;
+    private int date;
+    private long maxFileSize;
+
+
+  //**************************************************************************
+  //** Constructor
+  //**************************************************************************
+  /** Creates a new instance of Logger. */
+
+    public Logger(File logDir, long maxFileSize) {
+        this.logDir = logDir;
+        this.date = -1;
+        this.maxFileSize = maxFileSize;
+    }
+
+
+  //**************************************************************************
+  //** log
+  //**************************************************************************
+  /** Writes to the pool */
+
+    public static void log(String client, HttpServletRequest request) {
+        
+        StringBuffer str = new StringBuffer();
+        str.append("New Request From: " + client + "\r\n");
+        str.append(request.getMethod() + ": " + request.getURL() + "\r\n");
+        str.append("TimeStamp: " + new java.util.Date() + "\r\n");
+        str.append("\r\n");
+        str.append(request.toString());
+        log(str.toString());
+    }
+
+    public static void log(Exception e){
+        String s = e.getClass().getName();
+        s = s.substring(s.lastIndexOf(".")+1);
+        String message = e.getLocalizedMessage();
+        String error = (message != null) ? (s + ": " + message) : s;
+        log(error);
+    }
+
+    
+    private static void log(String str){
+        if (str!=null){
+            if (!str.endsWith("\r\n")) str+= "\r\n";
+            synchronized (pool) {
+               pool.add(pool.size(), str);
+               pool.notifyAll();
+            }
+        }
+    }
+
+
+
+
+  //**************************************************************************
+  //** Run
+  //**************************************************************************
+  /** Used to remove RequestHeader from the queue and write it to a file.
+   */
+    public void run() {
+        while (true) {
+
+            String request = null;
+            synchronized (pool) {
+                while (pool.isEmpty()) {
+                  try {
+                    pool.wait();
+                  }
+                  catch (InterruptedException e) {
+                      break;
+                  }
+                }
+                request = (String) pool.remove(0);
+            }
+
+if (request.contains("User-Agent: EMR Bootstrap Service")) continue;
+
+            try{                
+                byte[] b = request.getBytes();
+                if (b.length>8*1024){ 
+                    if (request.length()>250) request = request.substring(0, 250);
+                    request += "...\r\n";
+                    b = request.getBytes();
+                }
+                ByteBuffer output = ByteBuffer.allocateDirect(b.length);
+                output.put(b);
+                output.flip();
+                getFileChannel().write(output);
+            }
+            catch(Exception e){
+                //e.printStackTrace();
+            }
+
+        }
+    }
+
+
+    private FileChannel getFileChannel() throws Exception {
+        
+        int date = Integer.parseInt(new SimpleDateFormat("yyyyMMdd").format(new java.util.Date()));
+        if (date>this.date){
+            this.date = date;
+
+            if (outputFile!=null) outputFile.close();
+            if (outChannel!=null) outChannel.close();
+
+            File file = new File(logDir, this.date + ".log");
+            outputFile = new FileOutputStream(file, true);
+            outChannel = outputFile.getChannel();
+        }
+        else{
+            File file = new File(logDir, this.date + ".log");
+            if (file.length()>maxFileSize){ 
+                if (outputFile!=null) outputFile.close();
+                if (outChannel!=null) outChannel.close();                
+                throw new Exception("Log file too big: " + file.length() + " vs " + maxFileSize);
+            }
+        }
+        return outChannel;
+    }
+
+}
