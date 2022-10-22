a585ffb69ba5d9450fd1be63b31e47e1a57c0543
pborissow <peter.borissow@yahoo.com>
2022-10-22 22:42:27 +0000

    - Added new readLine() method and new methods for processing byte order mark in the CSV class
    git-svn-id: svn://192.168.0.80/JavaXT/javaxt-express@1437 2c7b0aa6-e0b2-3c4e-bb4a-8b65b6c465ff

--- src/javaxt/express/utils/CSV.java
+++ src/javaxt/express/utils/CSV.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 public class CSV {
 
-    public static final String UTF8_BOM = "\uFEFF";
+    //public static final String UTF8_BOM = "\uFEFF";
 
   //**************************************************************************
   //** Columns
@@ -102,6 +102,105 @@ public class CSV {
             str.append(c);
         }
         return str.toString();
+    }
+
+
+  //**************************************************************************
+  //** readLine
+  //**************************************************************************
+  /** Returns a substring for the given data, ending at the first line break
+   *  that is not inside a quote. Example usage:
+    <pre>
+
+      //Get input stream
+        javaxt.io.File file; //create file!
+        java.io.InputStream is = file.getInputStream();
+
+      //Read header
+        String header = CSV.readLine(is);
+        int bom = CSV.getByteOrderMark(header);
+        if (bom>-1) header = header.substring(bom);
+        console.log(header);
+
+      //Read rows
+        String row;
+        while (!(row=CSV.readLine(is)).isEmpty()){
+            console.log(row);
+        }
+
+      //Close input stream
+        is.close();
+    </pre>
+   */
+    public static String readLine(java.io.InputStream is) throws java.io.IOException {
+
+        StringBuilder str = new StringBuilder();
+        boolean insideDoubleQuotes = false;
+        int i = 0;
+        while((i=is.read())!=-1) {
+            char c = (char) i;
+
+            if ((c=='\r' || c=='\n') && str.length()==0) continue;
+
+            if (c=='"'){
+                if (insideDoubleQuotes) insideDoubleQuotes = false;
+                else insideDoubleQuotes = true;
+            }
+
+            if (c=='\r' || c=='\n'){
+                if (!insideDoubleQuotes) break;
+            }
+            str.append(c);
+        }
+        return str.toString();
+    }
+
+
+  //**************************************************************************
+  //** getByteOrderMark
+  //**************************************************************************
+  /** Returns end position of the Byte Order Mark (BOM). Example usage:
+    <pre>
+        int bom = CSV.getByteOrderMark(header);
+        if (bom>-1) header = header.substring(bom);
+    </pre>
+   */
+    public static int getByteOrderMark(String str){
+
+        if (str.length()<2) return -1;
+
+        int a=-1, b=-1, c=-1, d=-1;
+        if (str.length()>1){
+            a = (int) str.charAt(0);
+            b = (int) str.charAt(1);
+            if (a==254 && b==255) return 2; //UTF-16 (BE)
+            if (b==255 && b==254) return 2; //UTF-16 (LE)
+        }
+
+        if (str.length()>2){
+            c = (int) str.charAt(2);
+            if (a==239 && b==187 && c==191) return 3; //UTF-8
+            if (a==43 && b==47 && c==118) return 3; //UTF-7
+            if (a==247 && b==100 && c==76) return 3; //UTF-1
+        }
+
+        if (str.length()>3){
+            d = (int) str.charAt(3);
+            if (a==0 && b==0 && c==254 && d==255) return 4; //UTF-32 (BE)
+            if (a==255 && b==254 && c==0 && d==0) return 4; //UTF-32 (LE)
+        }
+
+        return -1;
+    }
+
+
+  //**************************************************************************
+  //** startsWithByteOrderMark
+  //**************************************************************************
+  /** Returns true if the given string starts with a Byte Order Mark (BOM)
+   */
+    public static boolean startsWithByteOrderMark(String str){
+        return getByteOrderMark(str)>-1;
     }
 
 
