52abd264029ec9f3382009d200515b34cf5fc431
pborissow <peter.borissow@yahoo.com>
2024-01-11 15:26:53 -0500

    Updated FileManager to use the DateUtils class

--- src/javaxt/express/FileManager.java
+++ src/javaxt/express/FileManager.java
@@ -1,4 +1,5 @@
 package javaxt.express;
+import javaxt.express.utils.DateUtils;
 import javaxt.http.servlet.HttpServletRequest;
 import javaxt.http.servlet.HttpServletResponse;
 import static javaxt.utils.Console.console;
@@ -29,8 +30,6 @@ public class FileManager {
 
     private javaxt.io.Directory web;
     private String[] welcomeFiles = new String[]{"index.html", "index.htm", "default.htm"};
-    private static final String z = "GMT";
-    private static final TimeZone tz = TimeZone.getTimeZone(z);
 
 
   //**************************************************************************
@@ -346,6 +345,7 @@ public class FileManager {
   //**************************************************************************
   /** Sends a given string to the client. Transparently handles caching using
    *  "ETag" and "Last-Modified" headers.
+   *  @param date UTC date in milliseconds since January 1, 1970, 00:00:00 UTC
    */
     public void sendResponse(String html, long date, HttpServletRequest request,
         HttpServletResponse response) throws IOException {
@@ -355,7 +355,7 @@ public class FileManager {
         long size = html.length();
         String eTag = "W/\"" + size + "-" + date + "\"";
         response.setHeader("ETag", eTag);
-        response.setHeader("Last-Modified", getDate(date)); //Sat, 23 Oct 2010 13:04:28 GMT
+        response.setHeader("Last-Modified", DateUtils.getDate(date)); //Sat, 23 Oct 2010 13:04:28 GMT
         //this.setHeader("Cache-Control", "max-age=315360000");
         //this.setHeader("Expires", "Sun, 30 Sep 2018 16:23:15 GMT  ");
 
@@ -571,73 +571,6 @@ public class FileManager {
     }
 
 
-  //**************************************************************************
-  //** getDate
-  //**************************************************************************
-  /** Used to convert a date to a string (e.g. "Mon, 20 Feb 2012 07:22:20 EST").
-   *  This method does not rely on the java.text.SimpleDateFormat for
-   *  performance reasons.
-   */
-    private static String getDate(Calendar cal){
 
-        if (!cal.getTimeZone().equals(tz)){
-            cal = (java.util.Calendar) cal.clone();
-            cal.setTimeZone(tz);
-        }
-
-        StringBuffer str = new StringBuffer(29);
-        switch(cal.get(Calendar.DAY_OF_WEEK)){
-            case Calendar.MONDAY:    str.append("Mon, "); break;
-            case Calendar.TUESDAY:   str.append("Tue, "); break;
-            case Calendar.WEDNESDAY: str.append("Wed, "); break;
-            case Calendar.THURSDAY:  str.append("Thu, "); break;
-            case Calendar.FRIDAY:    str.append("Fri, "); break;
-            case Calendar.SATURDAY:  str.append("Sat, "); break;
-            case Calendar.SUNDAY:    str.append("Sun, "); break;
-        }
-
-        int i = cal.get(Calendar.DAY_OF_MONTH);
-        str.append(i<10 ? "0"+i : i);
-
-        switch (cal.get(Calendar.MONTH)) {
-            case Calendar.JANUARY:   str.append(" Jan "); break;
-            case Calendar.FEBRUARY:  str.append(" Feb "); break;
-            case Calendar.MARCH:     str.append(" Mar "); break;
-            case Calendar.APRIL:     str.append(" Apr "); break;
-            case Calendar.MAY:       str.append(" May "); break;
-            case Calendar.JUNE:      str.append(" Jun "); break;
-            case Calendar.JULY:      str.append(" Jul "); break;
-            case Calendar.AUGUST:    str.append(" Aug "); break;
-            case Calendar.SEPTEMBER: str.append(" Sep "); break;
-            case Calendar.OCTOBER:   str.append(" Oct "); break;
-            case Calendar.NOVEMBER:  str.append(" Nov "); break;
-            case Calendar.DECEMBER:  str.append(" Dec "); break;
-        }
-
-        str.append(cal.get(Calendar.YEAR));
-        str.append(" ");
-
-        i = cal.get(Calendar.HOUR_OF_DAY);
-        str.append(i<10 ? "0"+i+":" : i+":");
-
-        i = cal.get(Calendar.MINUTE);
-        str.append(i<10 ? "0"+i+":" : i+":");
-
-        i = cal.get(Calendar.SECOND);
-        str.append(i<10 ? "0"+i+" " : i+" ");
-
-        str.append(z);
-        return str.toString();
-
-        //new java.text.SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
-        //return f.format(date);
-    }
-
-
-    private String getDate(long milliseconds){
-        java.util.Calendar cal = java.util.Calendar.getInstance();
-        cal.setTimeInMillis(milliseconds);
-        return getDate(cal);
-    }
 
 }
